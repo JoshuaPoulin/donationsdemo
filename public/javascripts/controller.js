e45e7ff69d264a4386d8ef870289dc3229ba5b40
JoshuaPoulin <jpoulin87@gmail.com>
2016-06-10 12:19:44 -0600

    lots and lots of css

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,3 +1,21 @@
-app.controller('HomeController', ['$scope', '$http','$location', '$rootScope', function($scope, $http, $location, $rootScope){
-    $scope.testing = 'testing';
+app.controller('HomeController', ['$scope', '$interval', '$http', 
+  function($scope, $interval, $http){
+    $scope.data = {
+      "valueNum": 56,
+      "value": "$56",
+      "goal": "$125",
+      "remaining": "$69"
+    }
+    var stop;
+    $scope.progressValue = 0;
+    $scope.progress = function() {
+      stop = $interval(function() {
+        if ($scope.progressValue === $scope.data.valueNum) {
+          $interval.cancel(stop);
+        } else {
+          $scope.progressValue++;
+        }
+      }, 50);
+    }
+    angular.element(document).ready($scope.progress);
 }]);
